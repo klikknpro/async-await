bd4f436c1e9f487e33807c733e990b6147ac8b57
klikknpro <80091502+klikknpro@users.noreply.github.com>
2021-12-03 12:18:25 +0100

    Simplified async await

--- script.js
+++ script.js
@@ -1,10 +1,36 @@
-function init() {}
+const renderCountries = (countries) => {
+  let returnHtml = "";
+
+  for (const country of countries) {
+    returnHtml += `
+    <div>${country.name.official}</div>
+    `;
+  }
+  return returnHtml;
+};
+
+const renderFlags = (flags) => {
+  let returnHtml = "";
+
+  for (const flag of flags) {
+    returnHtml += `
+    <img src="${flag.flags.png}">
+    `;
+  }
+  return returnHtml;
+};
+
+async function init() {
+  const countryRes = await fetch("https://restcountries.com/v3.1/all");
+  const countryArr = await countryRes.json();
+
+  document
+    .getElementById("root")
+    .insertAdjacentHTML("beforeend", `<div id="countryNames">${renderCountries(countryArr)}</div>`);
+  document
+    .getElementById("root")
+    .insertAdjacentHTML("beforeend", `<div id="countryFlags">${renderFlags(countryArr)}</div>`);
+  // renderCountries(countryArr);
+}
 
 window.addEventListener("load", init);
-
-/*
-window.addEventListener("load", function(){
-});
-window.addEventListener("load", () => {
-});
-*/
